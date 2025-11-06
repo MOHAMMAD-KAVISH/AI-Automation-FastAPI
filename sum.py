9ca8f9952124e3fa7f06c611fa9588a3adca679c
MOHAMMAD KAVISH <119128166+MOHAMMAD-KAVISH@users.noreply.github.com>
2025-11-06 15:35:57 +0530

    Delete sum.py

--- sum.py
+++
@@ -1,6 +0,0 @@
-#write sum of to two numbers in python
-def sum_two_numbers(a, b):
-    return a + b
-# Example usage
-result = sum_two_numbers(3, 5)
-print("The sum is:", result)  # Output: The sum is: 8
